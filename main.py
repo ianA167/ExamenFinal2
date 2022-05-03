3edde82eec7da05ec9f2c53e899599a494efd6a3
ianA167 <100703769+ianA167@users.noreply.github.com>
2022-05-03 09:37:59 -0600

    Agregado archivo de selección de animales

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+animal_ingresado = input("ingrese su animal favorito:")
+print(animal_ingresado)
+animal_favorito="oso"
+if animal_ingresado == animal_favorito:
+    print("Mi animal favorito tambien es ese")
+else:
+    print("Mi animal favorito es el "+animal_favorito)
+
+
